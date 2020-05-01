eddfdcb9ba94cc39a6cea9e2cc45947c4bbcf658
Daniel Linares Gil <daniel44@main-pc.myhost>
2020-05-01 17:03:45 -0500

    cambios parametros

--- proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto2/GraficableSVG.java
+++ proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto2/GraficableSVG.java
@@ -4,10 +4,10 @@ package mx.unam.ciencias.edd.proyecto2;
  * Interfaz para Clases que se puedan graficar usando un SVG.
  * Todas las clases que implementen esta interfaz deben implementae un método para generar un SVG que de una representación grafica de la clase.
  */
-public interface GraficableSVG {
+public interface GraficableSVG<T> {
     
     /**
      * Método que provee una representación gráfica como un SVG de la clase.
      */
-    public void graficarSVG(double largo, double ancho);
+    public void graficarSVG();
 }
