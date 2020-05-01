4ed9bf4187fa7e09bfb72b454f2d812ac24eef4c
Daniel Linares Gil <daniel44@main-pc.myhost>
2020-05-01 16:49:26 -0500

    version 0.9.1

--- proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto2/DibujarMonticuloMinimo.java
+++ proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto2/DibujarMonticuloMinimo.java
@@ -6,8 +6,6 @@ import mx.unam.ciencias.edd.Coleccion;
 
 public class DibujarMonticuloMinimo<T extends ComparableIndexable<T>> extends DibujarArbolBinarioCompleto<T> {
 
-    private MonticuloMinimo<T> monticulo;
-
     public DibujarMonticuloMinimo(Coleccion<T> coleccion) {
         super(new MonticuloMinimo<>(coleccion));
     }
